397f669ffd8369534f11521c4acaa15fcaffce95
jfVivanco <149414251+jfVivanco@users.noreply.github.com>
2023-11-08 22:10:42 -0600

    Autor.h

--- Autor.h
+++ Autor.h
@@ -1,17 +1,13 @@
-// Autor.h
-#ifndef AUTOR_H
-#define AUTOR_H
+#define AUTOR_H // Define AUTOR_H
 #include <string>
 
+// Define una clase de autor con dos atributos privados y un método público
 class Autor {
 public:
     Autor(const std::string& nombre, const std::string& genero);
     void info();
 
 private:
-    std::string nombre;
-    std::string genero;
+    std::string nombre; // Atributo para el nombre del autor
+    std::string genero; // Atributo para el genero del autor
 };
-
-#endif
-
