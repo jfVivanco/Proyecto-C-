048ab3ab3fd0d07c01b3a7fbae287b1738911caf
jfVivanco <149414251+jfVivanco@users.noreply.github.com>
2023-11-15 19:39:03 -0600

    Update Libro.h

--- Libro.h
+++ Libro.h
@@ -1,16 +1,24 @@
 #ifndef LIBRO_H
-#define LIBRO_H // Define LIBRO_H
+#define LIBRO_H
+
 #include <string>
 
-// Define una clase Libro con dos atributos privados y un método público
 class Libro {
 public:
-    Libro(const std::string& titulo, const std::string& autor); // Constructor con dos parámetros
+    Libro(const std::string& titulo, const std::string& autor);
     void info();
 
+    // Getters
+    std::string getTitulo() const;
+    std::string getAutor() const;
+
+    // Setters
+    void setTitulo(const std::string& titulo);
+    void setAutor(const std::string& autor);
+
 private:
-    std::string titulo; // Atributo para el título del libro
-    std::string autor; // Atributo para el autor del libro
+    std::string titulo;
+    std::string autor;
 };
 
 #endif
