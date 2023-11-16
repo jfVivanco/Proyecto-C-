7ad4a7678f18fecf876255ca8fbd2f7488fd207a
jfVivanco <149414251+jfVivanco@users.noreply.github.com>
2023-11-15 19:46:36 -0600

    Update Biblioteca.h

--- Biblioteca.h
+++ Biblioteca.h
@@ -9,9 +9,16 @@ class Biblioteca {
 public:
     Biblioteca(const std::string& nombre, Bibliotecario bibliotecario);
 
+    // Getters y setters
+    std::string getNombre() const;
+    void setNombre(const std::string& nuevoNombre);
+    Bibliotecario getBibliotecario() const;
+    void setBibliotecario(const Bibliotecario& nuevoBibliotecario);
+
     // Operaciones de la biblioteca
     void agregarLibro(const Libro& libro);
     void mostrarInventario() const;
+    void mostrarAutores() const;
 
 private:
     std::string nombre;
