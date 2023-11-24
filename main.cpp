7cf4eb6e7521692d6e39248d7be2087963f49972
jfVivanco <149414251+jfVivanco@users.noreply.github.com>
2023-11-24 01:10:18 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <vector>
-#include "Libro.h"
+#include "Libro_concreto.h"
 #include "Coleccion.h"
 #include "Seccion.h"
 
 int main() {
-    // Crear un libro
-    Libro libro("El principito", "Antoine de Saint-Exupéry", 100, "Infantil", 80, true);
+    // Crear un objeto de Libro_concreto en lugar de un objeto Libro
+    Libro_concreto libro("El principito", "Antoine de Saint-Exupéry", 100, "Infantil", 80);
 
     // Crear una colección
     Coleccion coleccion("Historias Cortas", "Varios Autores", "Varios Géneros", 200, "Ruta/De/La/Coleccion");
 
-    // Crear una sección
+    // Crear una sección con un vector de nombres de colecciones
     std::vector<std::string> nombresColecciones = {"Colección1", "Colección2", "Colección3"};
     Seccion seccion("Sección Principal", "Editor", 150, "Varios Géneros", nombresColecciones, 120);
 
     // Mostrar información
-    cout << "Información del Libro:" << endl;
-    libro.info();  // Llamada al método info() en el objeto libro
+    std::cout << "Información del Libro:" << std::endl;
+    libro.info();
 
-    cout << "\nInformación de la Colección:" << endl;
+    std::cout << "\nInformación de la Colección:" << std::endl;
     coleccion.info();
 
-    cout << "\nInformación de la Sección:" << endl;
+    std::cout << "\nInformación de la Sección:" << std::endl;
     seccion.info();
 
     return 0;
