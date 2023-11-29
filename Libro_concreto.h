11249856d49bdf007645a59679d2b1768b1cb47c
jfVivanco <149414251+jfVivanco@users.noreply.github.com>
2023-11-28 23:40:47 -0600

    Update Libro_concreto.h

--- Libro_concreto.h
+++ Libro_concreto.h
@@ -19,21 +19,23 @@
 
 #include "Libro.h"
 
+using namespace std;
+
 // Clase que hereda de la clase base 'Libro'
 class Libro_concreto: public Libro {
 public:
     // Constructor que inicializa la clase base 'Libro'
-    Libro_concreto(std::string nm, std::string thr, int lks, std::string gnr, int pgs)
+    Libro_concreto(string nm, string thr, int lks, string gnr, int pgs)
         : Libro(nm, thr, lks, gnr, pgs) {}
 
     // Método de información que overridea la implementación en la clase base
     void info() override {
         // Muestra información detallada sobre el libro concreto
-        std::cout << "Nombre: " << get_name() << std::endl;
-        std::cout << "Autor: " << get_author() << std::endl;
-        std::cout << "Genero: " << get_genre() << std::endl;
-        std::cout << "Likes: " << get_likes() << std::endl;
-        std::cout << "Paginas: " << get_pages() << std::endl;
+        cout << "Nombre: " << get_name() << endl;
+        cout << "Autor: " << get_author() << endl;
+        cout << "Genero: " << get_genre() << endl;
+        cout << "Likes: " << get_likes() << endl;
+        cout << "Paginas: " << get_pages() << endl;
     }
 };
 
