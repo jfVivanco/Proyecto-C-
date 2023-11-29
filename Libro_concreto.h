7bb9ef0153c93de5525ec17e02ee278f1bd96beb
jfVivanco <149414251+jfVivanco@users.noreply.github.com>
2023-11-28 23:29:04 -0600

    Update Libro_concreto.h

--- Libro_concreto.h
+++ Libro_concreto.h
@@ -1,8 +1,25 @@
+/*
+ * Proyecto Progra
+ * Jose Fernando Vivanco Benavides
+ * A00838976
+ * 28/11/2023
+*/
+
+/*
+ *La clase Libro_concreto es una subclase de la clase base Libro y tiene la finalidad de representar un libro concreto 
+ *con información específica. Su constructor toma parámetros como el nombre del libro (nm), el autor (thr), 
+ *la cantidad de likes (lks), el género (gnr), y el número de páginas (pgs), inicializando la clase base Libro con estos valores.
+
+ *La clase implementa un método info() que sobrescribe la implementación de la clase base. Este método imprime por la consola 
+ *detalles detallados del libro concreto, incluyendo su nombre, autor, género, cantidad de likes y número de páginas.
+*/
+
 #ifndef LIBRO_CONCRETO_H
 #define LIBRO_CONCRETO_H
 
 #include "Libro.h"
 
+// Clase que hereda de la clase base 'Libro'
 class Libro_concreto: public Libro {
 public:
     // Constructor que inicializa la clase base 'Libro'
